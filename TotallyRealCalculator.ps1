76acd815b7f02517a94d0baf3530f9dae4bcf5fa
LoyaltoParkway <ryan.flaherty@parkwaywest.org>
2025-11-13 13:22:04 -0500

    Very real
    Source (Trust me bro)

---
+++ TotallyRealCalculator.ps1
@@ -0,0 +1,38 @@
+#
+Read-Host "what is your first number "
+
+#
+Read-Host "what is your second number "
+
+#
+$option = Read-Host "would you like to:
+1. add
+2. subtract
+3. multiply
+4. divide
+5. sacifice to the mesh network
+"
+Write-Host "$option"
+
+if (5 -eq $option) {
+    write-host "SACRIFICE"
+    Start-Process -FilePath "C:\Program Files\Cisco Packet Tracer 8.2.2\bin\PacketTracer.exe"
+}elseif (1,2,3,4 -eq $option) {
+    write-host "Then Do it yourself, "
+    calc.exe
+    calc.exe
+    calc.exe
+    calc.exe
+    calc.exe
+    calc.exe
+    calc.exe    
+    calc.exe
+    calc.exe
+    calc.exe
+    calc.exe
+    calc.exe
+}
+else {
+    
+}
+
